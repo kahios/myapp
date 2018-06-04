215f0e77a86e4063c75db494736c2b16b377d122
kahios <thaylton-veloso@hotmail.com>
2018-06-04 14:44:01 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+var http = require("http");
+
+const server = http.createServer(function (request, response) {
+    if (request.url == '/') {
+        response.end('<h1>Inicio</h1>')
+    }
+    else if (request.url == '/contatos'){
+        response.end('<h1>Contatos</h1>')
+    }
+    else if (request.url == '/sobre'){
+        response.end('<h1>Sobre</h1>')
+    }
+    else {
+        response.statusCode = 404;
+        response.end ('<h1>Not Found!</h1>')
+    }
+});
+
+server.listen(4000);
+
+console.log('Servidor executando em localhost:4000/');
